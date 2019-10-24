c3483ba0bb18ecdf1d308bd4b83d19d657a94850
Matthew C <m23.cullen@gmail.com>
2019-10-24 15:09:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,11 +13,14 @@ class App < Sinatra::Base
   end 
   
   get '/say/:number/:phrase' do 
+    @string = ""
     params[:number].to_i.times do 
       @string += "#{params[:phrase]}"
     end 
     @string
   end 
   
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    
 
 end
